03ea9395c4a7813640cb6183d676fdde4a6f03dd
Can Michael Hucko <118487985+canmike@users.noreply.github.com>
2025-01-28 00:37:37 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="mikegrad",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.19.0",
     ],
     author="Can Michael Hucko",
     author_email="c.michaelhucko@gmail.com",
-    description="A minimal autograd engine",
+    description="A minimal autograd framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/canmike/mikegrad",
